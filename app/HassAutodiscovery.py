ec2d8923045acb53c48d2df7f1dd0611c448cd2a
serotonie <68992449+serotonie@users.noreply.github.com>
2024-03-17 20:56:12 +0100

    Add temperature, daily, weekly, monthly and yearly energy

--- app/HassAutodiscovery.py
+++ app/HassAutodiscovery.py
@@ -28,6 +28,81 @@ def Advertise (client, PowerOne, topic):
     Payload = Payload | DeviceBase
 
     client.publish("homeassistant/sensor/"+PowerOne["serial_number"]+"/"+Payload["unique_id"]+"/config",payload=json.dumps(Payload), qos=0, retain=True)
+    
+    Payload = {
+        "name": "Température",
+        "unique_id": "temperature",
+        "state_topic": topic,
+        "unit_of_measurement": "°C",
+        "value_template": "{{ value_json.inverter_temperature | round(1)}}",
+        "device_class": "temperature",
+        "state_class": "measurement",
+        "icon": "mdi:sun-thermometer"
+    }
+
+    Payload = Payload | DeviceBase
+
+    client.publish("homeassistant/sensor/"+PowerOne["serial_number"]+"/"+Payload["unique_id"]+"/config",payload=json.dumps(Payload), qos=0, retain=True)
+
+    Payload = {
+        "name": "Daily Production",
+        "unique_id": "daily_production",
+        "state_topic": "solar/1",
+        "unit_of_measurement": "kWh",
+        "value_template": "{{ value_json.daily_energy | round(1)}}",
+        "device_class": "energy",
+        "state_class": "total_increasing",
+        "icon": "mdi:solar-power-variant"
+    }
+
+    Payload = Payload | DeviceBase
+
+    client.publish("homeassistant/sensor/"+PowerOne["serial_number"]+"/"+Payload["unique_id"]+"/config",payload=json.dumps(Payload), qos=0, retain=True)
+
+    Payload = {
+        "name": "Weekly Production",
+        "unique_id": "weekly_production",
+        "state_topic": "solar/1",
+        "unit_of_measurement": "kWh",
+        "value_template": "{{ value_json.weekly_energy | round(1)}}",
+        "device_class": "energy",
+        "state_class": "total_increasing",
+        "icon": "mdi:solar-power-variant"
+    }
+
+    Payload = Payload | DeviceBase
+
+    client.publish("homeassistant/sensor/"+PowerOne["serial_number"]+"/"+Payload["unique_id"]+"/config",payload=json.dumps(Payload), qos=0, retain=True)    
+
+    Payload = {
+        "name": "Monthly Production",
+        "unique_id": "monthly_production",
+        "state_topic": "solar/1",
+        "unit_of_measurement": "kWh",
+        "value_template": "{{ value_json.monthly_energy | round(1)}}",
+        "device_class": "energy",
+        "state_class": "total_increasing",
+        "icon": "mdi:solar-power-variant"
+    }
+
+    Payload = Payload | DeviceBase
+
+    client.publish("homeassistant/sensor/"+PowerOne["serial_number"]+"/"+Payload["unique_id"]+"/config",payload=json.dumps(Payload), qos=0, retain=True)
+
+    Payload = {
+        "name": "Yearly Production",
+        "unique_id": "yearly_production",
+        "state_topic": "solar/1",
+        "unit_of_measurement": "kWh",
+        "value_template": "{{ value_json.yearly_energy | round(1)}}",
+        "device_class": "energy",
+        "state_class": "total_increasing",
+        "icon": "mdi:solar-power-variant"
+    }
+
+    Payload = Payload | DeviceBase
+
+    client.publish("homeassistant/sensor/"+PowerOne["serial_number"]+"/"+Payload["unique_id"]+"/config",payload=json.dumps(Payload), qos=0, retain=True)
 
     Payload = {
         "name": "Total Production",
